f47530b8c5891c6f506c15017c94f54acfcda674
sgold <ragaxus@gmail.com>
2019-03-29 11:14:22 -0500

    Added communication with Google Sheet

---
+++ WERToGoogle/GoogleSheet.cs
@@ -0,0 +1,62 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace WERToGoogle
+{
+    class GoogleSheet
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+        static string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static string ApplicationName = "WERToGoogle";
+        SheetsService service;
+        String spreadsheetId = "1kKD6qzhSqu0Kx4MuPjYNImJuHJjmAcpFj-ixA2kM4Ws";
+        String range = "TestData!A2:C";
+
+        public GoogleSheet()
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_id.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+        public void WriteValues(List<IList<object>> values) {
+            ValueRange body = new ValueRange();
+            body.MajorDimension = "ROWS";
+            body.Values = values;
+            // Define request parameters.
+            SpreadsheetsResource.ValuesResource.AppendRequest request =
+                    service.Spreadsheets.Values.Append(body, spreadsheetId, range);
+            request.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+
+            AppendValuesResponse response = request.Execute();
+        }
+    }
+}
--- WERToGoogle/Program.cs
+++ WERToGoogle/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace WERToGoogle
@@ -11,7 +8,19 @@ namespace WERToGoogle
     {
         static void Main(string[] args)
         {
+            Dictionary<long, string> playerDict = new Dictionary<long, string>();
+            List<IList<object>> playerValues;
+
             string filePath = @"C:\Users\sgold\Downloads\EnrolledPlayers-example.xml";
+            AddPlayersInFileToDictionary(playerDict, filePath);
+            ConvertPlayerDictionaryToTable(playerDict, out playerValues);
+
+            GoogleSheet sheet = new GoogleSheet();
+            sheet.WriteValues(playerValues);
+        }
+
+        private static void AddPlayersInFileToDictionary(Dictionary<long, string> playerDict, string filePath)
+        {
             XmlSerializer xmlSerializer = new XmlSerializer(typeof(@event));
             System.IO.StreamReader reader = new System.IO.StreamReader(filePath);
             @event @event = (@event)xmlSerializer.Deserialize(reader);
@@ -19,7 +28,20 @@ namespace WERToGoogle
             {
                 string playerName = player.name;
                 long playerDciNumber = player.players.player.dciNumber;
-                Console.WriteLine(String.Format("{0,-50} {1,-20}",playerName,playerDciNumber));
+                if (!playerDict.ContainsKey(playerDciNumber)) playerDict.Add(playerDciNumber, playerName);
+            }
+        }
+
+        private static void ConvertPlayerDictionaryToTable(Dictionary<long, string> playerDict, out List<IList<object>> playerValues)
+        {
+            playerValues = new List<IList<object>>();
+
+            foreach (var player in playerDict)
+            {
+                List<object> list = new List<object>();
+                list.Add(player.Key);
+                list.Add(player.Value);
+                playerValues.Add(list);
             }
         }
     }
