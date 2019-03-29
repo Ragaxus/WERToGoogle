73d6d93067a0d5aa6cd6b3419e1874c4451f49da
Ragaxus <ragaxus@gmail.com>
2019-03-28 23:33:51 -0500

    Created -- includes auto-genned class file from xsd

--- WERToGoogle/Program.cs
+++ WERToGoogle/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace WERToGoogle
 {
@@ -10,6 +11,16 @@ namespace WERToGoogle
     {
         static void Main(string[] args)
         {
+            string filePath = @"C:\Users\sgold\Downloads\EnrolledPlayers-example.xml";
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(@event));
+            System.IO.StreamReader reader = new System.IO.StreamReader(filePath);
+            @event @event = (@event)xmlSerializer.Deserialize(reader);
+            foreach (var player in @event.teams)
+            {
+                string playerName = player.name;
+                long playerDciNumber = player.players.player.dciNumber;
+                Console.WriteLine(String.Format("{0,-50} {1,-20}",playerName,playerDciNumber));
+            }
         }
     }
 }
