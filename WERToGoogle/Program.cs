1c17bdeb9d638b64186ec5e93409dc35b24ebab3
Ragaxus <ragaxus@gmail.com>
2019-03-30 00:47:54 -0500

    Split name field into lastname, firstname columns

--- WERToGoogle/Program.cs
+++ WERToGoogle/Program.cs
@@ -10,7 +10,7 @@ namespace WERToGoogle
     {
         static void Main(string[] args)
         {
-            Dictionary<long, string> playerDict = new Dictionary<long, string>();
+            Dictionary<long, string[]> playerDict = new Dictionary<long, string[]>();
             List<IList<object>> playerValues;
             if (args.Length < 1) {
                 Console.Write("To use this program, drag and drop the file or folder to convert onto the icon.");
@@ -40,20 +40,20 @@ namespace WERToGoogle
             sheet.WriteValues(playerValues);
         }
 
-        private static void AddPlayersInFileToDictionary(Dictionary<long, string> playerDict, string filePath)
+        private static void AddPlayersInFileToDictionary(Dictionary<long, string[]> playerDict, string filePath)
         {
             XmlSerializer xmlSerializer = new XmlSerializer(typeof(@event));
             System.IO.StreamReader reader = new System.IO.StreamReader(filePath);
             @event @event = (@event)xmlSerializer.Deserialize(reader);
             foreach (var player in @event.teams)
-            {
+            { 
                 string playerName = player.name;
                 long playerDciNumber = player.players.player.dciNumber;
-                if (!playerDict.ContainsKey(playerDciNumber)) playerDict.Add(playerDciNumber, playerName);
+                if (!playerDict.ContainsKey(playerDciNumber)) playerDict.Add(playerDciNumber, playerName.Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries));
             }
         }
 
-        private static void ConvertPlayerDictionaryToTable(Dictionary<long, string> playerDict, out List<IList<object>> playerValues)
+        private static void ConvertPlayerDictionaryToTable(Dictionary<long, string[]> playerDict, out List<IList<object>> playerValues)
         {
             playerValues = new List<IList<object>>();
 
@@ -61,7 +61,7 @@ namespace WERToGoogle
             {
                 List<object> list = new List<object>();
                 list.Add(player.Key);
-                list.Add(player.Value);
+                foreach (var namepart in player.Value) { list.Add(namepart); };
                 playerValues.Add(list);
             }
         }
